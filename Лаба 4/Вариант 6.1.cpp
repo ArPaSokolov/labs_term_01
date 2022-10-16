4a452fd67c44689dba951ec599680698225feb18
Арсений <usokolof@gmail.com>
2022-10-16 17:40:32 +0300

    lab 4

---
+++ Лаба 4/Вариант 6.1.cpp
@@ -0,0 +1,59 @@
+﻿/* 1.	Дана последовательность натуральных чисел 
+{aj}j=1...n (n<=10000). 
+Если в последовательности нет ни одного простого числа, 
+упорядочить последовательность по неубыванию.*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter the number of elements:\n";
+    int numberOfElements;
+    int primeNumber = 0;
+    bool allPrime = true; // флаг - все числа простые
+    std::cin >> numberOfElements;
+    int mas[10000];
+    std::cout << "Enter the elements:\n";
+    for (int i = 0; i < numberOfElements; i++)
+    {
+        std::cin >> mas[i];
+        for (int j = 2; j < sqrt(mas[i]); j++)
+        {
+            if ((mas[i] % j) == 0 or mas[i] == 1)
+            {
+                allPrime = false; // нашли непростое число
+                primeNumber++; // количество непростых
+                break;
+            }
+        }
+
+    }
+    if (allPrime == false) // флаг сработал - нет простых чисел
+    {
+        for (int k = 0; k < numberOfElements - 1; k++)
+        {
+            for (int l = k + 1; l < numberOfElements; l++)
+            {
+                if (mas[k] > mas[l])
+                {
+                    std::swap(mas[k], mas[l]);
+                }
+            }
+        }
+        std::cout << "The array:\n";
+        for (int h = 0; h < numberOfElements; h++)
+        {
+            std::cout << mas[h] << " ";
+        }
+    }
+    else // флаг сработал
+    {
+        std::cout << "The array:\n";
+        for (int h = 0; h < numberOfElements; h++)
+        {
+            std::cout << mas[h] << " ";
+        }
+    }
+
+    return 0;
+}
---
+++ Лаба 4/Вариант 6.3.cpp
@@ -0,0 +1,56 @@
+
+/*	ƒана целочисленна€ матрица {Aij}i=1..n,j=1..m (n,m<=100). 
+Ќайти строку с наибольшим произведением элементов
+и заменить все элементы этой строки этим произведением.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    cout << "Enter the number of columns:\n";
+    int N,S;  // кол-во столбцов и строк
+    int maxProizvLine; // номер строки с максимальным произведением
+    int maxProizv = 0;    // максимальное произ
+    cin >> N;
+    cout << "Enter the number of line:\n";
+    cin >> S;
+    double matrix[100][100];
+    // вводим матрицу
+    cout << "Enter the matrix:\n";
+    for (int i = 0; i < S; i++)
+    {
+        int Proizv = 1;
+        for (int j = 0; j < N; j++)
+        {
+            cin >> matrix[i][j];
+            Proizv *= matrix[i][j];
+        }
+        if (maxProizv < Proizv)
+        {
+            maxProizv = Proizv;
+            maxProizvLine = i;
+        }
+    }
+    
+    // выводим матрицу
+    cout << "\n";
+    cout << "The matrix:";
+    for (int l = 0; l < S; l++)
+    {
+        cout << "\n";
+        for (int k = 0; k < N; k++)
+        {
+            if (l == maxProizvLine)
+            {
+                cout << (matrix[l][k] = maxProizv) << " ";
+            }
+            else
+            {
+                cout << matrix[l][k] << " ";
+            }
+        }
+    }
+    return 0;
+}
